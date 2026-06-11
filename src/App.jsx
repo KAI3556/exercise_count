4805cf5dfe3d29a4fbf7bfa96e0340b9f8f57f42
Thanh <phamgiathanh1611@gmail.com>
2026-06-11 14:48:01 +0700

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import Header from './Khung/Header.jsx'
-import WorkoutScreen from './screens/WorkoutScreen.jsx'
-import './App.css'
+import "./App.css"
+import {WorkoutSequence} from "./workout-sequence/workout-sequence.jsx";
 
 function App() {
     return (
-        <Header>
-            <WorkoutScreen />
-        </Header>
+        <WorkoutSequence />
+        // <WorkoutScreen />
+        // <Header>
+        // </Header>
     );
 }
 
