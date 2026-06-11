8313fdcc58f5821fa381b711c9b6d0f15a1335e6
Thanh <phamgiathanh1611@gmail.com>
2026-06-11 15:33:52 +0700

    main

---
+++ src/components/Controls.jsx
@@ -0,0 +1,30 @@
+import styles from "../workout/workout-screen.module.css";
+
+export const Controls = ({ playing, onPlayPause, onPrev, onNext, hasNext }) => {
+    return (
+        <div className={styles.controls}>
+            <button
+                className={styles.ctrlBtn}
+                onClick={onPrev}
+                disabled={!onPrev}
+            >
+                ⏮
+            </button>
+
+            <button
+                className={`${styles.ctrlBtn} ${styles.ctrlBtnPlay}`}
+                onClick={onPlayPause}
+            >
+                {playing ? '⏸' : '▶'}
+            </button>
+
+            <button
+                className={styles.ctrlBtn}
+                onClick={onNext}
+                disabled={!hasNext}
+            >
+                ⏭
+            </button>
+        </div>
+    )
+}
